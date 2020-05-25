3d7612f5c19d9a96dee5e4abd7a7880ca4bbc670
Wishvanath SMG <anandwishvanath@gmail.com>
2020-05-25 22:10:56 +0530

    Updates

---
+++ precache-manifest.8d7939020e6715c3b3785f385935930a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce846bc0a02d8f671a14009c25e0c634",
+    "url": "/walkar-demo/index.html"
+  },
+  {
+    "revision": "226d3ee6a54e86f7e026",
+    "url": "/walkar-demo/static/css/main.6f59bde3.chunk.css"
+  },
+  {
+    "revision": "f9a3acdd34626d411c61",
+    "url": "/walkar-demo/static/js/2.295acbd1.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/walkar-demo/static/js/2.295acbd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226d3ee6a54e86f7e026",
+    "url": "/walkar-demo/static/js/main.0c6090d9.chunk.js"
+  },
+  {
+    "revision": "5e330024526e5f5f7890",
+    "url": "/walkar-demo/static/js/runtime-main.9833b054.js"
+  }
+]);
